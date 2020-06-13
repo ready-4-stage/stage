6581a7ad4465bd5fa2abed484c20218bf7d5c1b3
yannickkirschen <yannickkirschen@protonmail.com>
2020-06-13 13:29:23 +0200

    Add JavaDoc for FileUtil
    Signed-off-by: yannickkirschen <yannickkirschen@protonmail.com>

--- stage-common/src/main/java/stage/common/FileUtil.java
+++ stage-common/src/main/java/stage/common/FileUtil.java
@@ -6,6 +6,16 @@ import java.nio.charset.StandardCharsets;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.io.IOUtils;
 
+/**
+ * {@link FileUtil} provides some handy functions for working with files.
+ *
+ * @author Julian Drees
+ * @author Tobias Fuchs
+ * @author Yannick Kirschen
+ * @author Cevin Steve Oehne
+ * @author Tobias Tappert
+ * @since 1.0.0
+ */
 @Log4j2
 public final class FileUtil {
     private static final ClassLoader LOADER = FileUtil.class.getClassLoader();
@@ -13,6 +23,13 @@ public final class FileUtil {
     private FileUtil() {
     }
 
+    /**
+     * Reads a (text) file entirely into a string. The charset of the file must
+     * be UTF-8.
+     *
+     * @param file The complete name of the file.
+     * @return The file's content as a string.
+     */
     public static String readFile(String file) {
         try (InputStream inputStream = LOADER.getResourceAsStream(file)) {
             assert inputStream != null;

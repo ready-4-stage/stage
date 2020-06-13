f139d9e81d493c97f2a1c4873a4754979d81cf93
yannickkirschen <yannickkirschen@protonmail.com>
2020-06-13 13:30:30 +0200

    Add JavaDoc for StartupException
    Signed-off-by: yannickkirschen <yannickkirschen@protonmail.com>

--- stage-common/src/main/java/stage/common/StartupException.java
+++ stage-common/src/main/java/stage/common/StartupException.java
@@ -1,4 +1,14 @@
 package stage.common;
 
+/**
+ * Indicates that the startup of the server produced an error.
+ *
+ * @author Julian Drees
+ * @author Tobias Fuchs
+ * @author Yannick Kirschen
+ * @author Cevin Steve Oehne
+ * @author Tobias Tappert
+ * @since 1.0.0
+ */
 public class StartupException extends RuntimeException {
 }

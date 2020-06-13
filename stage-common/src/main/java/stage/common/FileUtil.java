904bfb7bd207668e4caf9fae7d2fc5b8ac757a37
yannickkirschen <yannickkirschen@protonmail.com>
2020-06-13 13:29:06 +0200

    Make InputStream close with try-with
    Signed-off-by: yannickkirschen <yannickkirschen@protonmail.com>

--- stage-common/src/main/java/stage/common/FileUtil.java
+++ stage-common/src/main/java/stage/common/FileUtil.java
@@ -2,18 +2,19 @@ package stage.common;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.io.IOUtils;
 
 @Log4j2
 public final class FileUtil {
+    private static final ClassLoader LOADER = FileUtil.class.getClassLoader();
+
     private FileUtil() {
     }
 
     public static String readFile(String file) {
-        try {
-            InputStream inputStream = FileUtil.class.getClassLoader().getResourceAsStream(
-                file);
+        try (InputStream inputStream = LOADER.getResourceAsStream(file)) {
             assert inputStream != null;
             return IOUtils.toString(inputStream, StandardCharsets.UTF_8.name());
         } catch (IOException e) {

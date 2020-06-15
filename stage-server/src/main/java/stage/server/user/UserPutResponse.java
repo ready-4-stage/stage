5ce406fab04924ca8117805a361103c9395994c9
Julian Drees <julian-drees@gmx.de>
2020-06-15 20:02:42 +0200

    Added UserService manipulation methods & UserController

---
+++ stage-server/src/main/java/stage/server/user/UserPutResponse.java
@@ -0,0 +1,16 @@
+package stage.server.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * // TODO description
+ *
+ * @author Julian Drees
+ * @since 15.06.2020
+ */
+@Data
+@AllArgsConstructor
+public class UserPutResponse {
+    private Integer id;
+}
